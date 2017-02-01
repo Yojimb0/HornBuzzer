8f7815543a40048e39da42baea155a35fe148b63
Pierre Pointereau <pierreyves.pointereau@news.com.au>
2017-02-02 00:35:37 +1100

    Adding socket communication Removing hardcoded password 404 Route Timing since restart event

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var currentWinner = '';
+var respondingTeams = [];
+var timestamp = process.hrtime();
 
 /* Express setup
 ---------------------------------------*/
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
-
 
 /* Express routes
 ---------------------------------------*/
@@ -23,15 +23,42 @@ app.get('/team/:team', function(req, res){
 	res.render('team', {team: req.params.team});
 });
 
+app.get('/admin/:password', function(req, res){
+	if(req.params.password === (process.env.PASSWORD)){
+		console.log('[server.js - route admin] Password matching', req.params.password);
+		return res.render('admin');
+	}
+	console.log('[server.js - route admin] Password not matching => 404');
+	res.render('404');
+});
+
+app.get('*', function(req, res){
+	res.status(404);
+	res.render('404');
+});
 
 /* WebSocket communication
 ---------------------------------------*/
 io.on('connection', function(socket){
 	console.log('[server.js - socket] New connection');
 	socket.on('buzz', function(team){
-		if(!currentWinner) currentWinner = team;
-		console.log('[server.js - socket] buzz from team:' + team + ' currentWinner:' + currentWinner);
-		socket.emit('winner', currentWinner);
+		console.log('[server.js - socket(buzz)] team:' + team)
+		console.log('[server.js - socket(buzz)] respondingTeams:', respondingTeams)
+		if(respondingTeams.find(o => o.team === team)) return console.log('[server.js - socket(buzz)] team:' + team + ' already counted');
+		respondingTeams.push({
+			team: team,
+			time: process.hrtime(timestamp)[0] + '.' + Math.round(process.hrtime(timestamp)[1]/10000000) + 's'
+		});
+		console.log('[server.js - socket(buzz)] buzz from team:' + team);
+		socket.broadcast.emit('winner', respondingTeams);
+		socket.emit('winner', respondingTeams);
+	});
+	socket.on('reset', function(){
+		console.log('[server.js - socket(reset)]')
+		socket.broadcast.emit('resetClients');
+		socket.emit('resetClients');
+		timestamp = process.hrtime();
+		respondingTeams = [];
 	});
 });
 
@@ -39,5 +66,5 @@ io.on('connection', function(socket){
 /* Server
 ---------------------------------------*/
 http.listen(process.env.PORT || 5000, function(){
-	console.log('[server.js - listen] port:' + process.env.PORT || 5000);
+	console.log('[server.js - listen] port:' + (process.env.PORT || 5000));
 });
